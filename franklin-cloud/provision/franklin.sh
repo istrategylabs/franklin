7c9acdbf9c8be6176f0693ece9b0ae84705aeea6
Julian Gindi <juliangindi@gmail.com>
2016-01-26 14:50:59 -0500

    focusing on static for now since that's the immediate need

--- franklin-cloud/provision/franklin.sh
+++ franklin-cloud/provision/franklin.sh
@@ -3,8 +3,8 @@
 #
 
 sudo apt-get update
-sudo apt-get -y install apt-transport-https
-sudo apt-get -y install vim
+sudo apt-get -y install apt-transport-https vim libreadline-dev libncurses5-dev libpcre3-dev \
+    libssl-dev perl make build-essential upstart
 
 #
 # Create a 'Franklin' user, with the password 'franklin'
@@ -39,8 +39,29 @@ rm /usr/local/go1.5.2.linux-amd64.tar.gz
 # Add 'Go' to the user Franklin's PATH
 sudo -u franklin -H sh -c 'echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile'
 
+
 #
-# Install Postgres
+# Install and configure nginx and openresty
 #
 
-sudo apt-get install -y postgresql postgresql-contrib
+sudo -u franklin -H sh -c 'echo "export PATH=$PATH:/sbin" >> $HOME/.profile'
+
+wget https://openresty.org/download/ngx_openresty-1.9.7.1.tar.gz
+tar -xzf ngx_openresty-1.9.7.1.tar.gz
+
+cd ngx_openresty-1.9.7.1 && ./configure --with-luajit && make && sudo make install
+
+sudo -u franklin -H sh -c 'echo "export PATH=/usr/local/openresty/nginx/sbin:$PATH" >> $HOME/.profile'
+
+#
+# Set-up all the fun static stuff
+#
+
+sudo mkdir -p /var/nginx/conf
+sudo cp /franklin-static/nginx/nginx.conf /var/nginx/conf/
+sudo cp /franklin-static/nginx/router.lua /var/nginx/conf/
+sudo cp /franklin-static/nginx/mime.types /var/nginx/conf/
+sudo cp /franklin-static/upstart/franklin-static.conf /etc/init/resty.conf
+
+# Start the service
+sudo service resty start
